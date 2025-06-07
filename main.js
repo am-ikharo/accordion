9c92502ef76e1abad7e42f680e88138c9c98788a
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-08 00:44:24 +0100

    created html, css and javascrit files

---
+++ main.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id : '1',
+        question : "what are accordiom components?",
+        answer: "Arccordion components are user interface elements"
+    },
+    {
+        id : '2',
+        question : "what are they used for?",
+        answer: "They are commonly employed in various contexts,"
+    },
+    {
+        id : '3',
+        question : "Accodion as a musical instrument",
+        answer: "the arccordion is a musical instrument with a key"
+    },
+    {
+        id : '4',
+        question : "Can i create an accordion component with?",
+        answer: "Yes ofcourse, its is very possible to create an accordion "
+    }
+
+]
