e592638164ef13f30c8e97f2fd6cd0bee4ce1046
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-08 01:07:40 +0100

    created the questions div in the main.js to be rendered in the html

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 const data = [
-    {
-        id : '1',
-        question : "what are accordiom components?",
-        answer: "Arccordion components are user interface elements"
-    },
-    {
-        id : '2',
-        question : "what are they used for?",
-        answer: "They are commonly employed in various contexts,"
-    },
-    {
-        id : '3',
-        question : "Accodion as a musical instrument",
-        answer: "the arccordion is a musical instrument with a key"
-    },
-    {
-        id : '4',
-        question : "Can i create an accordion component with?",
-        answer: "Yes ofcourse, its is very possible to create an accordion "
-    }
+  {
+    id: "1",
+    question: "what are accordiom components?",
+    answer: "Arccordion components are user interface elements",
+  },
+  {
+    id: "2",
+    question: "what are they used for?",
+    answer: "They are commonly employed in various contexts,",
+  },
+  {
+    id: "3",
+    question: "Accodion as a musical instrument",
+    answer: "the arccordion is a musical instrument with a key",
+  },
+  {
+    id: "4",
+    question: "Can i create an accordion component with?",
+    answer: "Yes ofcourse, its is very possible to create an accordion ",
+  },
+];
 
-]
+const accordionWrapper = document.querySelector(".accordion");
+
+function createAccodionData() {
+  accordionWrapper.innerHTML = data.map(
+    (dataItem) => `
+        <div class="accordion_item">
+        <div class="accordion_title">
+        <h3>${dataItem.question}</h3>
+        <i class="fa-solid fa-arrow-up"></i>
+        </div>
+        </div>
+        `
+  ).join(" ");
+}
+
+createAccodionData();
